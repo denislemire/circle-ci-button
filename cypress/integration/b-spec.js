ff283ec6a19a144b16e872c061eb740d2fb883d0
Denis Lemire <denis@lemire.name>
2022-11-11 15:02:48 -0700

    Pay attention, hidden != none, you're not a CSS n00b

--- cypress/integration/b-spec.js
+++ cypress/integration/b-spec.js
@@ -11,7 +11,7 @@ describe('b-spec', () => {
 
     it('Check h1 display', () => {
       cy.get('#HiddenText')
-        .should('have.css', 'display', 'hidden')
+        .should('have.css', 'display', 'none')
     })
   })
 })
