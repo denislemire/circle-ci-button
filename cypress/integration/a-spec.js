70687bfdaaffb1c6fdb1880be3a6c6f5187783c7
Denis Lemire <denis@lemire.name>
2022-11-11 15:47:38 -0700

    Merge and squash cypress branch cleanly, now that experiment is done.

---
+++ cypress/integration/a-spec.js
@@ -0,0 +1,18 @@
+describe('b-spec', () => {
+  describe('page', () => {
+    beforeEach(() => {
+      debugger
+      cy.visit(Cypress.env('HOST') || 'htdocs/index.html')
+    })
+
+    it('Click my button', () => {
+      cy.get('#HiddenText')
+        .should('have.attr','style','display: none')
+
+      cy.get('#MyButton').click() // Click on button
+
+      cy.get('#HiddenText')
+        .should('have.attr','style','display: block;')
+    })
+  })
+})
